9d1fe3ae84e9f2e24fcbdc01ca153ef6c6654cf0
Alexander Schrab <alexander.schrab@gmail.com>
2022-12-02 21:25:35 +0100

    Update pid.js

--- pid.js
+++ pid.js
@@ -1,10 +1,13 @@
 // Declare PID class
 class PID {
   // Declare PID constants
-  constructor(Kp, Ki, Kd) {
+  constructor(Kp, Ki, Kd, minOutput, maxOutput) {
     this.Kp = Kp; // Proportional gain
     this.Ki = Ki; // Integral gain
     this.Kd = Kd; // Derivative gain
+
+    this.minOutput = minOutput; // Minimum output value
+    this.maxOutput = maxOutput; // Maximum output value
 
     // Initialize loop variables
     this.error = 0;
@@ -13,6 +16,16 @@ class PID {
     this.derivative = 0;
     this.time = 0;
     this.previousTime = 0;
+  }
+
+  // Clamp output value to specified range
+  clampOutput(output) {
+    return Math.max(this.minOutput, Math.min(this.maxOutput, output));
+  }
+
+  // Limit integral term to specified range
+  limitIntegral(integral) {
+    return Math.max(this.minOutput, Math.min(this.maxOutput, integral));
   }
 
   // PID loop function
@@ -31,6 +44,9 @@ class PID {
     // Calculate integral value
     this.integral = this.integral + this.error * dt;
 
+    // Limit integral term to specified range
+    this.integral = this.limitIntegral(this.integral);
+
     // Calculate derivative value
     if (dt > 0) {
       this.derivative = (this.error - this.previousError) / dt;
@@ -43,6 +59,9 @@ class PID {
       this.Kp * this.error +
       this.Ki * this.integral +
       this.Kd * this.derivative;
+
+    // Clamp output value to specified range
+    output = this.clampOutput(output);
 
     // Store error value and time for next iteration
     this.previousError = this.error;
